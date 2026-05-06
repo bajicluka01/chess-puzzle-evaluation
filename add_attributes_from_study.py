85adde19d2f92dc21dd5f9ddd40ff45c3c98725f
zala02 <zu0476@student.uni-lj.si>
2026-05-07 00:24:33 +0200

    vsi razn seemingly good-a

--- add_attributes_from_study.py
+++ add_attributes_from_study.py
@@ -11,7 +11,21 @@ m = 50
 stockfish = Stockfish(path="/usr/games/stockfish")
 
 
-def find_meaninful_moves(position, winning_side):
+def chebyshev_distance(move):
+    from_sq = move.from_square
+    to_sq = move.to_square
+
+    from_file = chess.square_file(from_sq)
+    from_rank = chess.square_rank(from_sq)
+
+    to_file = chess.square_file(to_sq)
+    to_rank = chess.square_rank(to_sq)
+
+    return max(abs(from_file - to_file), abs(from_rank - to_rank))
+
+
+
+def find_meaningful_moves(position, winning_side, level_one=False):
     """
     function that returns all the meaningful moves for the given position 
     """
@@ -20,7 +34,7 @@ def find_meaninful_moves(position, winning_side):
     if position.is_game_over():
         return [], []
     
-
+    
     # who's on the move
     turn = position.turn  # True = white, False = black
     #"""
@@ -100,6 +114,8 @@ def find_meaninful_moves(position, winning_side):
     #print(f"BEST EVAL: {best_eval}")
     # now do whatever they do in pseudocode
     meaningful_moves = []
+    winning_but_not_mating = 0
+
     for move, eval_cp in evaluations:
 
         # and now check based on who's supposed to win 
@@ -110,9 +126,24 @@ def find_meaninful_moves(position, winning_side):
             if winning_side == chess.WHITE:
                 if eval_cp >= w:
                     meaningful_moves.append(move)
+
+                    # winning and also mating ?
+                    if level_one:
+                        if abs(eval_cp) == 1500:            # ni najleps sam idc sue me
+                            winning_but_not_mating += 0
+                        else: 
+                            winning_but_not_mating += 1
             else:
                 if eval_cp <= -w:
                     meaningful_moves.append(move)
+
+                    # winning and also mating ?
+                    if level_one:
+                        if abs(eval_cp) == 1500:
+                            winning_but_not_mating += 0
+                        else: 
+                            winning_but_not_mating += 1
+
         else:
             # checks if a move is close enough to the best move 
             if abs(best_eval - eval_cp) <= m:
@@ -122,11 +153,11 @@ def find_meaninful_moves(position, winning_side):
     #print(f"meaningful moves: {meaningful_moves}")
     #print(f"we have {len(meaningful_moves)} meaningful moves")
 
-    return meaningful_moves
+    return meaningful_moves, winning_but_not_mating
 
 
 
-def number_of_meaningful_moves(data, max_levels, do_x_samples):
+def number_of_meaningful_moves(data, max_levels, do_x_samples, testing):
 
     # (?) idk how much we should put 
     # level 1 means all the meaningful moves for the winning side 
@@ -144,9 +175,21 @@ def number_of_meaningful_moves(data, max_levels, do_x_samples):
 
     for l in range(1, levels+1):
         data[f"branching_L{l}"] = 0.0
-        
 
     data["avg_branching"] = 0.0
+
+    for l in range(1, levels+1):
+        data[f"narrow_L{l}"] = 0        
+
+    for l in range(1, levels+1):
+        data[f"distance_L{l}"] = 0
+
+    for l in range(1, levels+1):
+        data[f"pieces_L{l}"] = 0
+
+    data["all_pieces_involved"] = 0
+
+    data["winning_no_mate"] = 0
 
     for idx, row in data.iterrows(): 
 
@@ -160,9 +203,34 @@ def number_of_meaningful_moves(data, max_levels, do_x_samples):
         # for the level 1, that is a starting position
         # find all the meaningful moves and return them
         #print(f"LEVEL 1")
-        meaningful_moves = find_meaninful_moves(starting_position, winning_side)
+        meaningful_moves, winning_but_not_mating = find_meaningful_moves(starting_position, winning_side, True)
         meaningful_moves_combs = [[move] for move in meaningful_moves]
         data.loc[idx, "meaningful_L1"] = len(meaningful_moves)
+        if winning_but_not_mating == False: 
+            data.loc[idx, "winning_no_mate"] = 0
+        else:
+            data.loc[idx, "winning_no_mate"] = 1
+
+        # narrow solutions
+        if len(meaningful_moves) == 1:
+            data.loc[idx, "narrow_L1"] = 1
+        else:
+            data.loc[idx, "narrow_L1"] = 0
+
+        # distances 
+        dist_L1 = sum(chebyshev_distance(m) for m in meaningful_moves)
+        data.loc[idx, "distance_L1"] = dist_L1
+
+        # different pieces involved
+        all_piece_types = set()
+
+        piece_types = set()
+        for move in meaningful_moves:
+            piece = starting_position.piece_at(move.from_square)
+            if piece is not None:
+                piece_types.add(piece.piece_type)
+                all_piece_types.add(piece.piece_type)
+        data.loc[idx, "pieces_L1"] = len(piece_types)
 
         # then for the other levels, play the found meaningful moves first
         # then find the meaningful moves in the final position
@@ -181,6 +249,15 @@ def number_of_meaningful_moves(data, max_levels, do_x_samples):
 
             new_combinations = []
 
+            # for narrow attribute 
+            narrow_count = 0
+
+            # for distance attribute
+            distance_count = 0
+
+            # for different pieces attribute
+            pieces_count = set()
+
             # go through all the combinations of moves
             for meaningful_move_comb in meaningful_moves_combs:
 
@@ -190,17 +267,32 @@ def number_of_meaningful_moves(data, max_levels, do_x_samples):
                     new_pos.push(meaningful_move)
 
                 # get the next meaningful moves
-                new_moves = find_meaninful_moves(new_pos, winning_side)
+                new_moves, _ = find_meaningful_moves(new_pos, winning_side)
+
+                distance_count += sum(chebyshev_distance(m) for m in new_moves)
+
+                if len(new_moves) == 1:
+                    narrow_count += 1
 
                 # and finally make combinations
                 # and hope that it aint gonna explode <3
                 for move in new_moves:
                     new_combinations.append(meaningful_move_comb + [move])
-            
+
+                    # also check the pieces in the meantime
+                    piece = new_pos.piece_at(move.from_square)
+                    if piece is not None:
+                        pieces_count.add(piece.piece_type)
+                        all_piece_types.add(piece.piece_type)
+
             meaningful_moves_combs = new_combinations
 
             data.loc[idx, f"meaningful_L{level+2}"] = len(meaningful_moves_combs)
+            data.loc[idx, f"narrow_L{level+2}"] = narrow_count
+            data.loc[idx, f"distance_L{level+2}"] = distance_count
+            data.loc[idx, f"pieces_L{level+2}"] = len(pieces_count)
 
+            data.loc[idx, "all_pieces_involved"] = len(all_piece_types)
 
         """
         print("Final combinations:")
@@ -236,11 +328,11 @@ def number_of_meaningful_moves(data, max_levels, do_x_samples):
         else:
             data.loc[idx, "avg_branching"] = 0.0
      
-        
-        print(f"row {idx} done")
+        if testing:
+            print(f"row {idx} done")
 
-        if idx > do_x_samples:
-            break
+            if idx > do_x_samples:
+                break
 
     # remove branching L1
     data = data.drop(columns=["branching_L1"], errors="ignore")
@@ -284,7 +376,7 @@ def dfs_possible_moves(position, total_num_of_levels, current_level, counts):
 
 
 
-def number_of_possible_moves(data, max_levels, do_x_samples):
+def number_of_possible_moves(data, max_levels, do_x_samples, testing):
 
     levels = max_levels
     for l in range(1, levels+1):
@@ -303,10 +395,51 @@ def number_of_possible_moves(data, max_levels, do_x_samples):
         for l in range(1, levels+1):
             data.loc[idx, f"possible_L{l}"] = counts[l]
 
-        print(f"row {idx} done")
+        if testing: 
+            print(f"row {idx} done")
 
-        if idx > do_x_samples:
-            break
+            if idx > do_x_samples:
+                break
+
+    return data
+
+
+
+
+def miscellaneous(data, max_levels, do_x_samples, testing):
+
+    levels = max_levels
+
+    # AllPossibleMoves - sum over possible moves
+    possible_cols = [f"possible_L{l}" for l in range(1, levels+1)]
+    data["all_possible_moves"] = data[possible_cols].sum(axis=1)
+
+    # AllNarrowSolutions - sum over narrow solutions
+    narrow_cols = [f"narrow_L{l}" for l in range(1, levels+1)]
+    data["all_narrow_solutions"] = data[narrow_cols].sum(axis=1)
+
+    # TreeSize - sum over meaningful moves
+    meaningful_cols = [f"meaningful_L{l}" for l in range(1, levels+1)]
+    data["tree_size"] = data[meaningful_cols].sum(axis=1)
+
+    # MoveRatio(L) - ratio between meaningful moves and all possible moves
+    for l in range(1, levels+1):
+        meaningful_col = f"meaningful_L{l}"
+        possible_col = f"possible_L{l}"
+        ratio_col = f"move_ratio_L{l}"
+
+        data[ratio_col] = data[meaningful_col] / data[possible_col]
+        data[ratio_col] = data[ratio_col].fillna(0.0)
+
+
+    # SumDistance - sum over distances
+    distance_cols = [f"distance_L{l}" for l in range(1, levels+1)]
+    data["sum_distance"] = data[distance_cols].sum(axis=1)
+
+    # AverageDistance
+    data["avg_distance"] = data["sum_distance"] / data["tree_size"]
+    data["avg_distance"] = data["avg_distance"].fillna(0.0)
+
 
     return data
 
@@ -320,20 +453,47 @@ if __name__ == '__main__':
 
 
 
-    # add attributes
-
-    levels = 3          # depth
-    do_x_samples = 5    # debugging and time consuming reasons -> comment in the function once you run whole dataset
+    levels = 3          # depth, if we're brave enough, 5 would be nice (be aware to change some logic in that case tho!!)
+    do_x_samples = 1    # debugging and time consuming reasons
+    testing = True      # DO FALSE ONCE YOU RUN ON ALL DATA
 
     
     # 1 Meaningful(L) -> for 3 levels
     # 4 Branching(L) -> for 3 levels (but 1 excluded)
     # 5 AverageBranching -> maybe not even useful since we then only have two levels lol
-    dataset_100k = number_of_meaningful_moves(dataset_100k, levels, do_x_samples)
+    # 6 NarrowSolution(L) 
+    # 11 Distance(L)
+    # 14 Pieces(L)
+    # 15 AllPiecesInvolved
+    # 17 WinningNoCheckmate
+    dataset_100k = number_of_meaningful_moves(dataset_100k, levels, do_x_samples, testing)
     #print(dataset_100k)
 
     
     # 2 PossibleMoves(L) 
-    # 3 AllPossibleMoves -> wtf je to? vrjetn sam sum teh levelov k jih pregledujemo, men se slis to precej useless 
-    dataset_100k = number_of_possible_moves(dataset_100k, levels, do_x_samples)
-    print(dataset_100k)
+    dataset_100k = number_of_possible_moves(dataset_100k, levels, do_x_samples, testing)
+    #print(dataset_100k)
+
+
+    # posebej funkcija, ker so sam sestevki po stolpcih ipd 
+    # 3 AllPossibleMoves
+    # 7 AllNarrowSolutions
+    # 8 TreeSize
+    # 9 MoveRatio
+    # 12 SumDistance
+    # 13 AverageDistance 
+    dataset_100k = miscellaneous(dataset_100k, levels, do_x_samples, testing)
+    print(dataset_100k.head(10))
+
+
+
+    # 16 PieceValueRatio -> MAMO ZE SAMI
+    # 18 BestMoveValue -> MAMO ZE SAMI
+    # 19 AverageBestMove(5) -> we aint doing level 5 <3 
+
+    # store new dataset
+    if testing == False:
+        dataset_100k.to_csv("dataset_100k_upgraded.csv", index=False)    
+
+
+
